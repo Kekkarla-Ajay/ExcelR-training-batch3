913a88d7262aa99e3e9cef4011e764f85e9b5909
Kekkarla-Ajay <ajaykekkarla@gmail.com>
2025-02-06 12:16:12 +0530

    to print array elements

---
+++ Demo065.java
@@ -0,0 +1,12 @@
+public class Demo065 {
+    public static void main(String[] args) {
+        // Initialize an array with some values
+        int[] arr = {10, 20, 30, 40, 50};
+
+        // Print each element of the array using a for loop
+        System.out.println("Array elements:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
