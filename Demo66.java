02865110a3974d9afa4e205c02c6ee5b701dfce7
Kekkarla-Ajay <ajaykekkarla@gmail.com>
2025-02-06 12:21:28 +0530

    to print index value of an array

---
+++ Demo66.java
@@ -0,0 +1,12 @@
+public class Demo066 {
+    public static void main(String[] args) {
+        // Initialize an array with some values
+        int[] arr = {10, 20, 30, 40, 50};
+
+        // Print the index and corresponding element of the array
+        System.out.println("Array indices and their corresponding elements:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Index: " + i + " -> Element: " + arr[i]);
+        }
+    }
+}
