7a0de7eed6b596ad7485f6ce4956d7a94a509d97
Kekkarla-Ajay <ajaykekkarla@gmail.com>
2025-02-10 12:48:39 +0530

    to reverse array elements

---
+++ Demo071.java
@@ -0,0 +1,38 @@
+public class Demo071{
+
+    
+    public static void reverseArray(int[] array) {
+        int start = 0;
+        int end = array.length - 1;
+
+        while (start < end) {
+            
+            int temp = array[start];
+            array[start] = array[end];
+            array[end] = temp;
+
+            
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        
+        int[] array = {1, 2, 3, 4, 5};
+
+        System.out.println("Original array:");
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        
+        reverseArray(array);
+
+        System.out.println("Reversed array:");
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+    }
+}
