fb0fa1629daace2d33ebe7f8ce049b7c9998c5a7
Kekkarla-Ajay <ajaykekkarla@gmail.com>
2025-02-12 12:51:33 +0530

    find the 2nd maximum number of an array

---
+++ Demo076.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Demo076{
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+
+        
+        int[] arr = new int[size];
+
+        
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = scanner.nextInt();
+        }
+}
+}
+
